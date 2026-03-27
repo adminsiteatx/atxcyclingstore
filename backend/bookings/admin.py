9651955cf28003c69d7a5a0d91b36b2f4999bd79
LETI-122642 <scaoz@iscte-iul.pt>
2026-03-27 22:42:45 +0000

    fix admin bookings

--- backend/bookings/admin.py
+++ backend/bookings/admin.py
@@ -1,4 +1,19 @@
 from django.contrib import admin
 from .models import Booking
 
-admin.site.register(Booking)
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = (
+        "nome",
+        "email",
+        "telemovel",
+        "marca_bicicleta",
+        "modelo_bicicleta",
+        "mensagem",
+        "data",
+    )
+
+    search_fields = ("nome", "email", "marca_bicicleta", "modelo_bicicleta")
+
+    list_filter = ("data",)
