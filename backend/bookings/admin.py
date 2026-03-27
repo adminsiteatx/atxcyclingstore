f1c5ecb77e2a56a3ca189b4951f826d0af20b2b1
LETI-122642 <scaoz@iscte-iul.pt>
2026-03-27 23:48:50 +0000

    minimal admin test

--- backend/bookings/admin.py
+++ backend/bookings/admin.py
@@ -1,32 +1,4 @@
 from django.contrib import admin
 from .models import Booking
 
-
-@admin.register(Booking)
-class BookingAdmin(admin.ModelAdmin):
-    list_display = (
-        "nome",
-        "email",
-        "telefone",
-        "servico",
-        "modelo_bike",
-        "mensagem",
-        "data",
-        "estado",
-        "created_at",
-    )
-
-    search_fields = (
-        "nome",
-        "email",
-        "telefone",
-        "modelo_bike",
-    )
-
-    list_filter = (
-        "servico",
-        "estado",
-        "created_at",
-    )
-
-    ordering = ("-created_at",)
+admin.site.register(Booking)
