2f86b75aaa1091648271c1fad0b237751115d80a
LETI-122642 <scaoz@iscte-iul.pt>
2026-03-28 00:10:18 +0000

    connect frontend to render backend

--- js/script.js
+++ js/script.js
@@ -261,7 +261,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         try {
 
-            const res = await fetch("http://127.0.0.1:8000/api/bookings/", {
+            const res = await fetch("https://atxcyclingstore.onrender.com/api/bookings/", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
