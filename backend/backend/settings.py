e85acde4a26c2247ea19084f8010d5402f8a8326
LETI-122642 <scaoz@iscte-iul.pt>
2026-03-29 16:05:44 +0100

    gmail ssl fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -115,15 +115,20 @@ STATIC_URL = 'static/'
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-EMAIL_HOST_USER = 'adminsiteatx@gmail.com'
-EMAIL_HOST_PASSWORD = 'bfcdsellkifytrtm'
+EMAIL_HOST = "smtp.gmail.com"
 
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
+
+EMAIL_HOST_USER = "adminsiteatx@gmail.com"
+EMAIL_HOST_PASSWORD = "bfcdsellkifytrtm"
+
+DEFAULT_FROM_EMAIL = "adminsiteatx@gmail.com"
+
+EMAIL_TIMEOUT = 30
 
 import os
 
